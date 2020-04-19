4df560e255f15f24969b55c04c04bd6232d5b2ac
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-04-19 16:45:25 +1200

    PATCH: fixes

--- src/Admin/WebPortfolioAdmin.php
+++ src/Admin/WebPortfolioAdmin.php
@@ -7,12 +7,12 @@ use Sunnysideup\WebPortfolio\Model\WebPortfolioItem;
 use Sunnysideup\WebPortfolio\Model\WebPortfolioAgent;
 use Sunnysideup\WebPortfolio\Model\WebPortfolioWhatWeDidDescriptor;
 
-class WebPortfolioAdmin extends ModelAdmin
+class TestMe extends ModelAdmin
 {
     private static $managed_models = [
         WebPortfolioItem::class,
-        WebPortfolioAgent::class,
         WebPortfolioWhatWeDidDescriptor::class,
+        WebPortfolioAgent::class,
     ];
 
     private static $url_segment = 'webportfolio'; // will be linked as /admin/products
